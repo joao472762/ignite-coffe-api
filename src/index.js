3c08a64efddc5a69446dec16584e3943b5683e28
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-07-24 08:05:20 -0300

    coffes api

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const cors = require('cors')
+const  express = require('express')
+
+const coffeList = require('../src/data')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.route('/api').get((req, res) => {
+    res.send(JSON.stringify(coffeList))
+})
+
+app.listen(3000,()=>{
+    console.log('hi i running in http://localhost:3000/')
+})
